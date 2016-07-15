122e1ee507123bbc6cbc1d5b376af56e4d68e5ec
william.lacy <williamlacy@uk.ibm.com>
2016-07-15 20:42:57 +0100

    first success pairing and connecting to yeelight

--- index.js
+++ index.js
@@ -1,53 +1,123 @@
 var util = require('util');
 
+
 var NobleDevice = require('noble-device');
+var SERVICE_UUID = '8e2f0cbd1a664b53ace6b494e25f87bd';
+var NOTIFY_UUID = '8f65073d9f574aaaafea397d19d5bbeb';
+var CONTROL_UUID = 'aa7d3f342d4f41e0807f52fbf8cf7443';
 
-var SERVICE_UUID          = 'fff0';
-var CONTROL_UUID          = 'fff1';
-var EFFECT_UUID           = 'fffc';
+function toHexString(byteArray) {
+    return byteArray.map(function(byte) {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('')
+}
 
-var YeelightBlue = function(peripheral) {
-  NobleDevice.call(this, peripheral);
+function toByteArray(hexString){
+    return hexString.match(/.{1,2}/g).map(hex => {
+        return parseInt(hex, 16);
+    });
+}
+
+const NOTIFY_STATUS = {
+    _NOTIFY: '4363',
+    NOT_AUTHED: '43631',
+    AUTHORIZED_PAIRED: '43632',
+    AUTHORIZED_DEVICE: '43634',
+    DISCONNECT_IMMINENT: '43637'
 };
 
-YeelightBlue.SCAN_UUIDS = [SERVICE_UUID];
+const CLIENT_UUID = '7207d94ecb9e4ec5be6eafa46ed1c07c';
 
-YeelightBlue.is = function(peripheral) {
-  var localName = peripheral.advertisement.localName;
+const commands = {
+    pair: 4367,
+    power: {
+        on: 434001,
+        off: 434002
+    },
+    colorAndBrightness: 4341
+};
+//response will be 01
+//press scene button to pair
+//then response is 02 if unpaired before and now paired
+//04 if paired before
 
-  return ((localName === undefined) || (localName === 'Yeelight Blu') || (localName === 'LightStrips'));
+function convertinttohex(int) {
+    return String("00" + int.toString(16)).slice(-2).toUpperCase();
+}
+
+function createwritecommand(command, args) {
+    return `${command}${args}000000000000000000000000000000000000`.slice(0, 36);
+}
+
+class YeelightLamp extends NobleDevice {
+    constructor(peripheral) {
+        super(peripheral)
+    }
+
+    turnOn(callback) {
+        this.writeYeelightStringCharacteristic(CONTROL_UUID, createwritecommand(commands.power.on, ''), callback);
+    }
+
+    turnOff(callback) {
+        this.writeYeelightStringCharacteristic(CONTROL_UUID, createwritecommand(commands.power.off, ''), callback);
+    }
+
+    setColorAndBrightness(red, green, blue, brightness, callback) {
+        const args = convertinttohex(red) + convertinttohex(green) + convertinttohex(blue) + convertinttohex(0) + convertinttohex(brightness);
+        return this.writeYeelightStringCharacteristic(CONTROL_UUID,
+            createwritecommand(commands.colorAndBrightness, args), callback);
+    }
+
+    writeYeelightStringCharacteristic(characteristicUuid, string, callback) {
+        this.writeDataCharacteristic(SERVICE_UUID, characteristicUuid, new Buffer(string, 'hex'), callback)
+    }
+
+}
+
+YeelightLamp.is = function (peripheral) {
+    var localName = peripheral.advertisement.localName;
+    return localName && (localName === 'Yeelight Blu' || localName === 'LightStrips' || /^XMCTD_.*$/.test(localName));
 };
 
-NobleDevice.Util.inherits(YeelightBlue, NobleDevice);
+YeelightLamp.discoverAndPair = function (callback) {
+    console.log('discovering device')
+    YeelightLamp.discover(function (yeelightLamp) {
+        console.log('found ' + yeelightLamp.uuid);
+        yeelightLamp.connectAndSetup(err => {
+            if (err) console.error(err);
+            console.log('connected.. getting notified of pairing changes')
+            var notifyCharacteristic = yeelightLamp._characteristics[SERVICE_UUID][NOTIFY_UUID];
+            notifyCharacteristic.on('read', function(data, isNotification) {
+                var pairingStatus = toHexString(data);
+                if (pairingStatus.substring(0, 4) === NOTIFY_STATUS._NOTIFY) {
+                    console.log('pairing notification received')
+                    if (pairingStatus.substring(0, 5) === NOTIFY_STATUS.AUTHORIZED_DEVICE) {
+                        console.log('pairing status is now authorized');
+                        callback(yeelightLamp);
+                    } else {
+                        console.log('not paired - code was ' + pairingStatus)
+                    }
+                }
+            });
+            notifyCharacteristic.notify(true, function(error) {
+                console.log('notification of changes to pairing on');
+                console.log('start pairing...');
+                yeelightLamp.writeYeelightStringCharacteristic(CONTROL_UUID, createwritecommand(commands.pair, CLIENT_UUID),
+                    err => {
+                        if (err) console.error(err);
+                        console.log('pair command sent successfully');
+                    });
+            });
 
-YeelightBlue.prototype.writeServiceStringCharacteristic = function(uuid, string, callback) {
-  this.writeStringCharacteristic(SERVICE_UUID, uuid, string, callback);
+            yeelightLamp.on('disconnect', function () {
+                console.log('disconnected!');
+                process.exit(0);
+            });
+        })
+    });
+
 };
 
-YeelightBlue.prototype.writeControlCharateristic = function(red, green, blue, brightness, callback) {
-  var command = util.format('%d,%d,%d,%d', red, green, blue, brightness);
+NobleDevice.Util.inherits(YeelightLamp, NobleDevice);
 
-  for (var i = command.length; i < 18; i++) {
-    command += ',';
-  }
-
-  this.writeServiceStringCharacteristic(CONTROL_UUID, command, callback);
-};
-
-YeelightBlue.prototype.turnOn = function(callback) {
-  this.writeControlCharateristic(255, 255, 255, 100, callback);
-};
-
-YeelightBlue.prototype.turnOff = function(callback) {
-  this.writeControlCharateristic(0, 0, 0, 0, callback);
-};
-
-YeelightBlue.prototype.setColorAndBrightness = function(red, green, blue, brightness, callback) {
-  this.writeControlCharateristic(red, green, blue, brightness, callback);
-};
-
-YeelightBlue.prototype.setGradualMode = function(on, callback) {
-  this.writeServiceStringCharacteristic(EFFECT_UUID, on ? 'TS' : 'TE', callback);
-};
-
-module.exports = YeelightBlue;
+module.exports = YeelightLamp;
--- test.js
+++ test.js
@@ -1,87 +1,67 @@
 var async = require('async');
 
-var YeelightBlue = require('./index');
+var YeelightLamp = require('./index');
 
-YeelightBlue.discover(function(yeelightBlue) {
-  console.log('found ' + yeelightBlue.uuid);
+YeelightLamp.discoverAndPair(yeelightLamp => {
+    function lightSequence(doneCallback) {
+        async.series([
+            function (callback) {
+                console.log('turnOn');
+                yeelightLamp.turnOn(function () {
+                    console.log('\tdelay');
+                    setTimeout(callback, 5000);
+                });
+            },
+            function (callback) {
+                console.log('setColorAndBrightness - red');
+                yeelightLamp.setColorAndBrightness(255, 0, 0, 100, function () {
+                    console.log('\tdelay');
+                    setTimeout(callback, 5000);
+                });
+            },
+            function (callback) {
+                console.log('setColorAndBrightness - green');
+                yeelightLamp.setColorAndBrightness(0, 255, 0, 100, function () {
+                    console.log('\tdelay');
+                    setTimeout(callback, 5000);
+                });
+            },
+            function (callback) {
+                console.log('setColorAndBrightness - blue');
+                yeelightLamp.setColorAndBrightness(0, 0, 255, 100, function () {
+                    console.log('\tdelay');
+                    setTimeout(callback, 5000);
+                });
+            },
+            function (callback) {
+                console.log('setColorAndBrightness - white + dim');
+                yeelightLamp.setColorAndBrightness(255, 255, 255, 50, function () {
+                    console.log('\tdelay');
+                    setTimeout(callback, 5000);
+                });
+            },
+            function (callback) {
+                console.log('turnOff');
+                yeelightLamp.turnOff(function () {
+                    callback();
+                });
+            },
+            function () {
+                doneCallback();
+            }
+        ]);
+    }
 
-  yeelightBlue.on('disconnect', function() {
-    console.log('disconnected!');
-    process.exit(0);
-  });
-
-  function lightSequence(doneCallback) {
     async.series([
-      function(callback) {
-        console.log('turnOn');
-        yeelightBlue.turnOn(function() {
-          console.log('\tdelay');
-          setTimeout(callback, 5000);
-        });
-      },
-      function(callback) {
-        console.log('setColorAndBrightness - red');
-        yeelightBlue.setColorAndBrightness(255, 0, 0, 100, function() {
-          console.log('\tdelay');
-          setTimeout(callback, 5000);
-        });
-      },
-      function(callback) {
-        console.log('setColorAndBrightness - green');
-        yeelightBlue.setColorAndBrightness(0, 255, 0, 100, function() {
-          console.log('\tdelay');
-          setTimeout(callback, 5000);
-        });
-      },
-      function(callback) {
-        console.log('setColorAndBrightness - blue');
-        yeelightBlue.setColorAndBrightness(0, 0, 255, 100, function() {
-          console.log('\tdelay');
-          setTimeout(callback, 5000);
-        });
-      },
-      function(callback) {
-        console.log('setColorAndBrightness - white + dim');
-        yeelightBlue.setColorAndBrightness(255, 255, 255, 50, function() {
-          console.log('\tdelay');
-          setTimeout(callback, 5000);
-        });
-      },
-      function(callback) {
-        console.log('turnOff');
-        yeelightBlue.turnOff(function() {
-          callback();
-        });
-      },
-      function() {
-        doneCallback();
-      }
+        function (callback) {
+            lightSequence(callback);
+        },
+        function (callback) {
+            lightSequence(callback);
+        },
+        function (callback) {
+            console.log('disconnect');
+            yeelightLamp.disconnect(callback);
+        }
     ]);
-  }
-
-  async.series([
-      function(callback) {
-        console.log('connectAndSetup');
-        yeelightBlue.connectAndSetup(callback);
-      },
-      function(callback) {
-        console.log('setGradualMode - true');
-        yeelightBlue.setGradualMode(true, callback);
-      },
-      function(callback) {
-        lightSequence(callback);
-      },
-      function(callback) {
-        console.log('setGradualMode - false');
-        yeelightBlue.setGradualMode(false, callback);
-      },
-      function(callback) {
-        lightSequence(callback);
-      },
-      function(callback) {
-        console.log('disconnect');
-        yeelightBlue.disconnect(callback);
-      }
-    ]
-  );
 });
